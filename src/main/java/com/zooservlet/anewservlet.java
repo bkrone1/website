6d8445725b8bd05e82d5eaf5b60081c1e21f2af6
Brendan <bkrone1@gmail.com>
2016-08-30 16:45:51 -0400

    website works

---
+++ src/main/java/com/zooservlet/anewservlet.java
@@ -0,0 +1,57 @@
+package com.zooservlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.goingtothezoo.Animals;
+import com.goingtothezoo.DAO;
+
+/**
+ * Servlet implementation class anewservlet
+ */
+@WebServlet("/anewservlet")
+public class anewservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public anewservlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Animals addTodb = new Animals();
+
+		addTodb.setName(request.getParameter("Name"));
+		addTodb.setSpecies(request.getParameter("Species"));
+		addTodb.setTypeOfCage(request.getParameter("TypeOfCage"));
+		addTodb.setFood(request.getParameter("Food"));
+		addTodb.setWeight(Float.parseFloat(request.getParameter("Weight")));
+
+		DAO.writeToDB(addTodb);
+
+	}
+
+}
